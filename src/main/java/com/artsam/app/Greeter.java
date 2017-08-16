baf16821fae793d3fb98cc824001e1d54214efc9
SamoshkinR-Tem <samoshkin-@ukr.net>
2017-08-16 12:21:35 +0300

    logic for getGreetingSentence() and its testing improved

--- src/main/java/com/artsam/app/Greeter.java
+++ src/main/java/com/artsam/app/Greeter.java
@@ -4,32 +4,41 @@ import com.artsam.app.tools.MyBundleControl;
 
 import java.time.Clock;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 class Greeter {
 
+    // period position numbers
     public static final int DEFAULT = -1;
     public static final int MORNING = 0;
     public static final int DAY = 1;
     public static final int EVENING = 2;
     public static final int NIGHT = 3;
 
+    // period start timestamps
     private static final LocalTime SIX = LocalTime.parse("06:00:00");
     private static final LocalTime NINE = LocalTime.parse("09:00:00");
     private static final LocalTime SEVENTEEN = LocalTime.parse("19:00:00");
     private static final LocalTime TWENTY_THREE = LocalTime.parse("23:00:00");
-    private static final LocalTime MIDNIGHT = LocalTime.parse("00:00:00");
 
+    // if U will add a period, its name has to be added into this array
+    // in position witch match a period pos number;
+    // actually, this array has to be saved in res file (like R.id... in android)
+    private static final String[] periodNames = new String[]{"app.good_morning",
+            "app.good_day","app.good_evening","app.good_night","app.default"};
 
     public void greet() {
-        App.logger.log();
+
         LocalTime nowUtcTime = LocalTime.now(Clock.systemUTC());
+
         int period = determineInterval(nowUtcTime);
-        App.logger.log("current period: " + period);
-        String greetingText = getRightSentence(period);
-        App.logger.log("text: " + greetingText + " just shown");
-        displayValue(greetingText);
+
+        String greetingText = getGreetingSentence(period);
+
+        System.out.println(greetingText);
+
     }
 
     public int determineInterval(LocalTime nowUtcTime) {
@@ -54,27 +63,13 @@ class Greeter {
         return period;
     }
 
-    public String getRightSentence(int period) {
-        App.logger.log();
+    public String getGreetingSentence(int period) {
 //        Locale.setDefault(new Locale("en", "US")); // if You want to try eng lang
+//        Locale.setDefault(new Locale("ru", "UA")); // if You want to try rus lang
+
         ResourceBundle bundle = ResourceBundle.getBundle(
                 "bundle", Locale.getDefault(), new MyBundleControl());
-        switch (period) {
-            case MORNING:
-                return bundle.getString("app.good_morning");
-            case DAY:
-                return bundle.getString("app.good_day");
-            case EVENING:
-                return bundle.getString("app.good_evening");
-            case NIGHT:
-                return bundle.getString("app.good_night");
-            default:
-                return bundle.getString("app.default");
-        }
-    }
 
-    public void displayValue(String message) {
-        System.out.println(message);
-        App.logger.log("Job done!");
+        return bundle.getString(periodNames[period]);
     }
 }
--- src/test/java/com/artsam/app/GreeterTest.java
+++ src/test/java/com/artsam/app/GreeterTest.java
@@ -14,82 +14,43 @@ import static org.junit.Assert.assertEquals;
 
 public class GreeterTest {
 
-    private int period;
-
-    public GreeterTest() {
-    }
-
-    @Before
-    public void executedBeforeEach() {
-        Greeter greeter = new Greeter();
-        LocalTime nowUtcTime = LocalTime.now(Clock.systemUTC());
-        period = greeter.determineInterval(nowUtcTime);
-    }
-
     @Test
     public void determineInterval() {
         try {
-            testDetermination(Greeter.NIGHT, LocalTime.parse("03:00:00"));
-            testDetermination(Greeter.MORNING, LocalTime.parse("06:00:00"));
-            testDetermination(Greeter.MORNING, LocalTime.parse("07:00:00"));
-            testDetermination(Greeter.DAY, LocalTime.parse("09:00:00"));
-            testDetermination(Greeter.DAY, LocalTime.parse("11:00:00"));
-            testDetermination(Greeter.EVENING, LocalTime.parse("19:00:00"));
-            testDetermination(Greeter.EVENING, LocalTime.parse("20:00:00"));
-            testDetermination(Greeter.NIGHT, LocalTime.parse("23:00:00"));
-            testDetermination(Greeter.NIGHT, LocalTime.parse("23:30:00"));
-            testDetermination(Greeter.DEFAULT, LocalTime.parse("24:00:00"));
+            testDet(Greeter.NIGHT, LocalTime.parse("03:00:00"));
+            testDet(Greeter.MORNING, LocalTime.parse("06:00:00"));
+            testDet(Greeter.MORNING, LocalTime.parse("07:00:00"));
+            testDet(Greeter.DAY, LocalTime.parse("09:00:00"));
+            testDet(Greeter.DAY, LocalTime.parse("11:00:00"));
+            testDet(Greeter.EVENING, LocalTime.parse("19:00:00"));
+            testDet(Greeter.EVENING, LocalTime.parse("20:00:00"));
+            testDet(Greeter.NIGHT, LocalTime.parse("23:00:00"));
+            testDet(Greeter.NIGHT, LocalTime.parse("23:30:00"));
+            testDet(Greeter.DEFAULT, LocalTime.parse("24:00:00"));
         } catch (DateTimeParseException e){
             e.printStackTrace();
         }
     }
 
-    private void testDetermination(int expected, LocalTime time) {
+    private void testDet(int expected, LocalTime time) {
         assertEquals(expected,
                 new Greeter().determineInterval(time));
     }
 
     @Test
-    public void getRightSentence() {
+    public void getGreetingSentence() {
+        testGetGS(new Locale("en", "US"),
+                Greeter.DAY, "Good day, World!"); // for eng lang
+
+        testGetGS(new Locale("ru", "RU"),
+                Greeter.NIGHT, "Доброй ночи, Мир!"); // for rus lang
+    }
+
+    private void testGetGS(Locale locale, int period, String expected) {
         try {
-            String greetText = new Greeter().getRightSentence(period);
-            if (Locale.getDefault().toString().matches("en_US")) {
-                switch (period) {
-                    case Greeter.MORNING:
-                        assertEquals("Good morning, World!", greetText);
-                        break;
-                    case Greeter.DAY:
-                        assertEquals("Good day, World!", greetText);
-                        break;
-                    case Greeter.EVENING:
-                        assertEquals("Good evening, World!", greetText);
-                        break;
-                    case Greeter.NIGHT:
-                        assertEquals("Good night, World!", greetText);
-                        break;
-                    case Greeter.DEFAULT:
-                        assertEquals("Witch World are You from?", greetText);
-                        break;
-                }
-            } else if (Locale.getDefault().toString().matches("ru_UA")) {
-                switch (period) {
-                    case Greeter.MORNING:
-                        assertEquals("Доброе утро, Мир!", greetText);
-                        break;
-                    case Greeter.DAY:
-                        assertEquals("Добрый день, Мир!", greetText);
-                        break;
-                    case Greeter.EVENING:
-                        assertEquals("Добрый вечер, Мир!", greetText);
-                        break;
-                    case Greeter.NIGHT:
-                        assertEquals("Доброй ночи, Мир!", greetText);
-                        break;
-                    case Greeter.DEFAULT:
-                        assertEquals("Вы с какой планеты?", greetText);
-                        break;
-                }
-            }
+            Locale.setDefault(locale);
+            assertEquals(expected,
+                    new Greeter().getGreetingSentence(period));
         } catch (Exception e) {
             MyLogger.getInstanceOf().log(Level.WARNING,
                     "MissingResourceException", e);
